19481a69b7d8586e73c781d74328db3fef996b40
priyanshu <priyanshu@gmail.com>
2024-08-21 19:35:39 +0530

    Done

---
+++ To-dO/script.js
@@ -0,0 +1,39 @@
+
+let todos = [];
+let ctr = 0;
+function render(todos) {
+    const todoList = document.getElementById("root");
+    todoList.innerHTML = '';
+    ctr = 0;
+    todos.forEach(todo => {
+        const div = document.createElement("div");
+        const h1 = document.createElement("h1");
+        const deleteButton = document.createElement("button")
+        deleteButton.setAttribute('onclick','deleteTodo('+ ctr + ')');
+        deleteButton.innerHTML = 'Delete'
+        h1.textContent = todo.title;
+        div.appendChild(h1);
+        div.appendChild(deleteButton);        
+        div.setAttribute('data-id',ctr);
+        todoList.appendChild(div);
+        ctr++;
+    });
+    
+
+}
+
+function addTodo() {
+
+    todos.push({
+        id: ctr,
+        title: document.getElementById("newInput").value,
+    })
+    render(todos);
+    ctr++;
+
+}
+
+function deleteTodo(index) {
+    todos.splice(index,1);
+    render(todos);
+}
