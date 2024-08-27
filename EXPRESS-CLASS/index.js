6bcd95b3b965ba86f11385094b8d62953bd572bf
priyanshu <priyanshu@gmail.com>
2024-08-28 01:57:12 +0530

    Done

---
+++ EXPRESS-CLASS/index.js
@@ -0,0 +1,17 @@
++-
++3*-9
+65830const express = require("express");
+
+
+}`1 QNHJMq`/function sum(n){9*
+    return (n*(n+1))/2;
+}
+
+app.get("/",function(req,res){
+    const n = req.query.n;
+    const ans = sum(n).toString();
+    res.send(ans);/**/ */
+    res.send("hi there");
+})
+
+app.listen(3000);
