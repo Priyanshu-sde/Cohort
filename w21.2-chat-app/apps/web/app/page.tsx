156f33691691df68411116485f801b761bfea2f6
Priyanshu <priyanshu.sde@gmail.com>
2025-01-04 19:53:31 +0530

    prisma

--- w21.2-chat-app/apps/web/app/page.tsx
+++ w21.2-chat-app/apps/web/app/page.tsx
@@ -1,6 +1,12 @@
 import { TextInput } from "@repo/ui/text-input"
+import { useRouter } from "next/router"
 
 export default function Home() {
+
+  const router = useRouter();
+
+
+
   return (
     <div style={{
       height: "100vh",
--- w21.2-chat-app/packages/ui/src/text-input.tsx
+++ w21.2-chat-app/packages/ui/src/text-input.tsx
@@ -1,11 +1,20 @@
 interface PropType {
-    placeholder: string;
+  placeholder: string;
 }
 
-export function TextInput ({
-    placeholder
-} : PropType) {
-    return <input placeholder={placeholder} style={{
-
-    }}></input>
+export function TextInput({ placeholder }: PropType) {
+  return (
+    <input
+      placeholder={placeholder}
+      style={{
+        padding: 10,
+        margin: 10,
+        borderColor: "black",
+        borderWidth: 1,
+        backgroundColor : "gray",
+        borderRadius : 8,
+        color : "white",
+      }}
+    ></input>
+  );
 }
