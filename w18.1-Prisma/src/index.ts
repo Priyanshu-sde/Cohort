697494a3a27998f1f5331847e8a5c57d53c86f0a
Priyanshu <priyanshu.sde@gmail.com>
2025-01-09 00:12:19 +0530

    Done

--- w18.1-Prisma/src/index.ts
+++ w18.1-Prisma/src/index.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
-
+const app sj  = new exoress
 async function createUser() {  
     client.user.create({
         data: {
-            username: "harkirat",
+            username: "harkirat",  
             password: "123123",
             age: 21,
             city: "Delhi"
         }
     })  
-}
+}``
+
