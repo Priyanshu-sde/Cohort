a068258bb75d69984cd59d847c681b421175dfa9
Priyanshu <priyanshu.sde@gmail.com>
2025-01-12 23:44:55 +0530

    Starting w22

--- w19-nextjs/app/page.tsx
+++ w19-nextjs/app/page.tsx
@@ -1,5 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return 
+  return (
+    <div className="text-lg w-screen h-screen items-center justify-center">
+      <div>Todo Application
+        <br />
+        <Link className="text-md border m-2" href="/signup">Signup</Link>
+        <br />
+        <Link className="text-md border m-2" href="/signin">Signin</Link>
+      </div>
+      </div>
+  )
 }
---
+++ w19-nextjs/app/signin/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+export default function Signin() {
+    return <div className="w-screen h-screen flex justify-center items-center">
+        <div className="border p-2">
+            <input type="text" placeholder="Username"></input>
+            <input type="password" placeholder="Password"></input>
+            <button>Signin</button>
+        </div>
+    </div>
+
+}
