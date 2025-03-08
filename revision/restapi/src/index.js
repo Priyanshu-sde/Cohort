cbeabc55fb505e3bff192450edbdb5f6bef90b6c
Priyanshu <priyanshu.sde@gmail.com>
2025-03-08 22:26:54 +0530

    Starting w22

--- revision/restapi/src/index.js
+++ revision/restapi/src/index.js
@@ -1,9 +1,56 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "itissecret";
 
 const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.post("/signup", (req,res) => {
+    const {username, password} = req.body;
+    users.push({
+        username,
+        password
+    })
+    res.send({
+        message: "You have signed up"
+    })
+    console.log(users);
+})
+
+app.post("/signin", (req,res) =>{
+    const {username, password} = req.body;
+    let founduser = null;
+    for(let i = 0;i < users.length;i++){
+        if(users[i].username === username && users[i].password === password){
+            founduser = users[i];
+        }
+    }
+
+    if(founduser) {
+        const token = jwt.sign({
+            username: username
+        }, JWT_SECRET);
+
+        res.json({
+            token: token,
+            message: "Signed in" 
+        })
+    }
+    else{
+        res.status(403).send({
+            message:"Invalid Username or password"
+        })
+    }
+    
+});
+
+
+
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on http://localhost:${PORT}')
+    console.log('Server is running on http://localhost:{$PORT}')
 })
