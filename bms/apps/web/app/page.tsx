91dddca5137c3affd1d666431fd10d6efaee744d
Priyanshu <priyanshu.sde@gmail.com>
2025-03-24 10:47:51 +0530

    Starting w25

--- bms/apps/web/app/page.tsx
+++ bms/apps/web/app/page.tsx
@@ -1,6 +1,7 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import {client} from "@repo/db/client";
 
 type Props = Omit<ImageProps, "src"> & {
   srcLight: string;
@@ -12,7 +13,7 @@ const ThemeImage = (props: Props) => {
 
   return (
     <>
-      <Image {...rest} src={srcLight} className="imgLight" />
+      <Image {...rest} s rc={srcLight} className="imgLight" />
       <Image {...rest} src={srcDark} className="imgDark" />
     </>
   );
